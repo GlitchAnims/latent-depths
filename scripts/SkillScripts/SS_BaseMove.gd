a53c0f61756021c8905902f83a2769cdf48de33d
GlitchAnims <38194089+GlitchAnims@users.noreply.github.com>
2026-09-25 21:01:20 -0300

    Base Move now walks and hits obstacles

--- scripts/SkillScripts/SS_BaseMove.gd
+++ scripts/SkillScripts/SS_BaseMove.gd
@@ -31,16 +31,37 @@ func FabricateSkillstructions(hex_from: Hex, hex_to: Hex) -> Array[SkillInstruct
 	var ins_ability: SkillInstruction = SkillInstruction.new()
 	ins_ability.timer = delay
 	ins_ability.ins_type = SkillInstruction.INS_TYPE.walk
-	ins_ability.coord_chosen_list.push_back(hex_to.coord)
+	
+	var hex_walk_list: Array[Hex] = GetHexWalkList(hex_to)
+	var coord_old: Vector2i = hex_from.coord
+	for hex_walk: Hex in hex_walk_list:
+		var coord_new: Vector2i = hex_walk.coord
+		var coord_vec: Vector2i = coord_new - coord_old
+		coord_old = coord_new
+		ins_ability.coord_chosen_list.push_back(coord_vec)
 	
 	var ins_cool: SkillInstruction = SkillInstruction.new()
 	ins_cool.timer = 2000
 	
 	return [ins_ability, ins_cool]
 
+
+func GetHexWalkList(hex_to: Hex) -> Array[Hex]:
+	var coord_to: Vector2i = hex_to.coord
+	var pathHex: PathHex = _distance_map[coord_to]
+	var hex_list: Array[Hex] = [hex_to]
+	
+	while pathHex != null and pathHex.dist > 1:
+		var coord_backwalk: Vector2i = pathHex.coord_from
+		var hex: Hex = GameData.hex_dict.get(coord_backwalk, null)
+		if hex != null: hex_list.push_front(hex)
+		pathHex = _distance_map.get(coord_backwalk, null)
+	
+	return hex_list
+
 func DoWorldHexWidgets(worldHex_dict: Dictionary[Vector2i, WorldHex]) -> void:
 	if _last_hex_to == null: return
-	
+	#GetPath(_last_hex_to)
 	var coord_to: Vector2i = _last_hex_to.coord
 	var pathHex: PathHex = _distance_map[coord_to]
 	
@@ -56,9 +77,23 @@ func DoWorldHexWidgets(worldHex_dict: Dictionary[Vector2i, WorldHex]) -> void:
 
 func Server_PerformInstruction(ins: SkillInstruction) -> void:
 	if ins.ins_type != SkillInstruction.INS_TYPE.walk: return
-	var coord: Vector2i = ins.coord_chosen_list[0]
-	Auth_Rem_ToClient_BaseWalkAbility(coord)
-	Auth_Rem_ToClient_BaseWalkAbility.rpc(coord)
+	var coord_dest: Vector2i = unit_ref.pos_hex
+	for coord_dir: Vector2i in ins.coord_chosen_list:
+		var coord_next_temp: Vector2i = coord_dest + coord_dir
+		var hex_next_temp: Hex = GameData.hex_dict.get(coord_next_temp, null)
+		if hex_next_temp == null: continue
+		if hex_next_temp.tile_flags & Hex.TILE_FLAGS.WALL: continue
+		# Don't know why no work, check later
+		#if not hex_next_temp.tile_flags & Hex.TILE_FLAGS.FREE: continue
+		var occupied: bool = false
+		for unit: Unit in GameData.unit_list_temp:
+			occupied = unit.pos_hex == coord_next_temp
+			if occupied: break
+		if occupied: continue
+		coord_dest = coord_next_temp
+	
+	Auth_Rem_ToClient_BaseWalkAbility(coord_dest)
+	Auth_Rem_ToClient_BaseWalkAbility.rpc(coord_dest)
 
 @rpc("authority", "call_remote", "reliable")
 func Auth_Rem_ToClient_BaseWalkAbility(coord: Vector2i) -> void:
